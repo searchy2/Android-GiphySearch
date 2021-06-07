1d397264d566e95a05ce3c6ff061550356a32531
Ray Li <ray.li.verified@gmail.com>
2021-06-07 16:32:00 -0500

    Update Gradle v3.2.0-beta05

--- library/src/main/java/stream/customgiphy/GiphyAdapter.java
+++ library/src/main/java/stream/customgiphy/GiphyAdapter.java
@@ -2,8 +2,8 @@ package stream.customgiphy;
 
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
